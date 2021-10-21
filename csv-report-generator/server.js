bee7f5e1b59f573bc83fe125d1c665401996e96d
Alex Ni <cl.alexni@gmail.com>
2021-10-20 20:44:44 -0500

    server handles clients post

--- csv-report-generator/server.js
+++ csv-report-generator/server.js
@@ -1,10 +1,27 @@
 var express = require('express');
+var path = require('path');
+var morgan = require('morgan');
 
-var app = express();
 var port = 3000;
+var app = express();
 
-app.listen(3000, () => {
-  console.log('listening on port: ', port);
+app.use(morgan('dev')); // this needs to be at the front in order to show
+app.use(express.urlencoded({ extended: true })); // content type urlencode, also provide body object to req?
+app.use(express.static(path.join(__dirname, 'client'))); //serve static files
+
+
+// app.get('/', (req, res) => {
+//   res.status(200).send('connected');
+// })
+
+app.post('/upload_json', (req, res) => {
+  console.log(req.body);
+  res.status(201).send();
+});
+
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
 })
 
 
