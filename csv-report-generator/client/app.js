5d2d64cc4a0eb7c09142ea7512908db02d11ceaf
Alex Ni <cl.alexni@gmail.com>
2021-10-21 14:39:07 -0500

    completed csv report generate without ajax

---
+++ csv-report-generator/client/app.js
@@ -0,0 +1,15 @@
+// var clicked = () => {
+//   let value = document.getElementById('textarea').value;
+
+//   axios.post('/upload_json', JSON.parse(value))
+//     .then((res) => {
+//       console.log(res);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     })
+  
+// }
+
+// prevent default submit
+// clear the input field
--- csv-report-generator/server.js
+++ csv-report-generator/server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
+//const bodyParser = require('body-parser');
+var {Parser} = require('json2csv');
 
 var port = 3000;
 var app = express();
 
 app.use(morgan('dev')); // this needs to be at the front in order to show
-app.use(express.urlencoded({ extended: true })); // content type urlencode, also provide body object to req?
+app.use(express.json()); //dont need this line?
+app.use(express.urlencoded({ extended: true })); // Content-Type urlencode, also provide body object to req? form data?
 app.use(express.static(path.join(__dirname, 'client'))); //serve static files
 
 
@@ -15,13 +18,50 @@ app.use(express.static(path.join(__dirname, 'client'))); //serve static files
 // })
 
 app.post('/upload_json', (req, res) => {
-  console.log(req.body);
-  res.status(201).send();
+  console.log(JSON.parse(req.body.data));
+  // generate csv
+  var csvData = csvGenerator(JSON.parse(req.body.data));
+  try {
+    var json2csv = new Parser({fields: csvData.fields});
+    var csvFile = json2csv.parse(csvData.records);
+    res.header('Content-Type', 'text/csv');
+    res.attachment('sales_report.csv');
+    res.status(201).send(csvFile);
+  } catch(err) {
+    console.err(err);
+    res.status(201).send();
+  }
 });
-
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
 })
 
+var csvGenerator = (data) => {
+  var fields = {};
+  var records = [];
+
+  var parseData = (data) => {
+    if (data.children && data.children.length === 0)  {
+      delete data.children;
+      for (var k in data) {
+        fields[k] = fields[k] || 0;
+      }
+      records.push(data);
+    } else {
+      var childrenData = data.children;
+      delete data.children;
+      for (var k in data) {
+        fields[k] = fields[k] || 0;
+      }
+      records.push(data);
+      childrenData.forEach(child => {
+        parseData(child);
+      });
+    }
+  }
+  parseData(data);
+  return {fields: Object.keys(fields),
+          records: records};
+}
 
